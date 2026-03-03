0b38d045423dd4a4e57ed42a8b1a384ac4a7f7ac
Alexey <lexac7078@gmail.com>
2026-03-03 15:38:27 +0500

    Файл заполнили кодом №4C-max и файл выводит максимальный возраст

--- num1.py
+++ num1.py
@@ -40,9 +40,13 @@ cursor = connection.cursor()
 ##average_age = cursor.fetchone()[0]
 ##print('Средний возраст пользователей:', average_age)
 
-cursor.execute('SELECT MIN(age)  FROM Users')
-min_age = cursor.fetchone()[0]
-print('Минимальный возраст среди пользователей:',min_age)
+##cursor.execute('SELECT MIN(age)  FROM Users')
+##min_age = cursor.fetchone()[0]
+##print('Минимальный возраст среди пользователей:',min_age)
+
+cursor.execute('SELECT MAX(age)  FROM Users')
+max_age = cursor.fetchone()[0]
+print('Максимальный возраст среди пользователей:',max_age)
 
 connection.commit()
 connection.close()
