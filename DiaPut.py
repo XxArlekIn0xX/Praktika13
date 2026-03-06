25594b4a7ff6e1676d45608c3da5c300d61dade4
Alexey <lexac7078@gmail.com>
2026-03-07 03:09:11 +0500

    Изменение диаг. окна

--- DiaPut.py
+++ DiaPut.py
@@ -14,14 +14,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog_Put(object):
     def setupUi(self, Dialog_Put):
         Dialog_Put.setObjectName("Dialog_Put")
-        Dialog_Put.resize(501, 302)
+        Dialog_Put.resize(501, 363)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog_Put)
-        self.buttonBox.setGeometry(QtCore.QRect(120, 260, 341, 32))
+        self.buttonBox.setGeometry(QtCore.QRect(110, 310, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.gridLayoutWidget = QtWidgets.QWidget(Dialog_Put)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(40, 30, 321, 227))
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(40, 30, 451, 271))
         self.gridLayoutWidget.setObjectName("gridLayoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
